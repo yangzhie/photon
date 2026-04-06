253d648fbbad634f3dd6b93f997cbb0eca94105d
yangzhie <arnavmalasi2212@gmail.com>
2026-04-06 19:15:47 +1000

    Initial commit.

---
+++ react-src/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <div className="text-3xl font-bold bg-red-400">
+                My Neutralinojs App!!
+            </div>
+        </>
+    );
+}
+
+export default App;
